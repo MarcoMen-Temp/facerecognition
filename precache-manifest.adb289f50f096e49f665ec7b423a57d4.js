c4047b80978db5eb528f9ad5bfaedc312e63ae5b
MarcoMen-Temp <mrc_men@yahoo.ie>
2020-07-01 19:55:34 +0100

    Updates

---
+++ precache-manifest.adb289f50f096e49f665ec7b423a57d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7de1913b37f55c97b2c844817e39b96",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "907c94af6562c5cc03bb",
+    "url": "/facerecognition/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3bac7da8d8a97e92655f",
+    "url": "/facerecognition/static/css/main.9477d721.chunk.css"
+  },
+  {
+    "revision": "907c94af6562c5cc03bb",
+    "url": "/facerecognition/static/js/2.6e05dfb1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.6e05dfb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bac7da8d8a97e92655f",
+    "url": "/facerecognition/static/js/main.17342303.chunk.js"
+  },
+  {
+    "revision": "fa6a11a9cd7eebd80802",
+    "url": "/facerecognition/static/js/runtime-main.1ad3eaac.js"
+  }
+]);
